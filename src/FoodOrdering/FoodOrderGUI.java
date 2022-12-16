939da635d4a5ace3da8a5416c7f894c346bb76e8
JohnChristianIdul <idulchristian@gmail.com>
2022-12-16 20:07:34 +0800

    Complete FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel pnlMain;
@@ -23,5 +25,45 @@ public class FoodOrderGUI extends JFrame{
         app.setSize(600,500);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
+    }
+
+    public FoodOrderGUI(){
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double total = 0;
+                if (cPizza.isSelected()) {
+                    total += 100;
+                }
+                if (cBurger.isSelected()) {
+                    total += 80;
+                }
+                if(cFries.isSelected()) {
+                    total += 65;
+                }
+                if(cTea.isSelected()) {
+                    total += 50;
+                }
+                if(cSoftDrinks.isSelected()) {
+                    total += 55;
+                }
+                if(cSundae.isSelected()) {
+                    total += 40;
+                }
+
+                if (rbNone.isSelected()) {
+                    JOptionPane.showMessageDialog(pnlMain, "The total price is Php " + String.format("%.2f",total));
+                } else if (rb5.isSelected()) {
+                    total -= total * .05;
+                    JOptionPane.showMessageDialog(pnlMain, "The total price is Php " + String.format("%.2f",total));
+                } else if (rb10.isSelected()) {
+                    total -= total * .10;
+                    JOptionPane.showMessageDialog(pnlMain, "The total price is Php " + String.format("%.2f",total));
+                } else {
+                    total -= total * .15;
+                    JOptionPane.showMessageDialog(pnlMain, "The total price is Php " + String.format("%.2f",total));
+                }
+            }
+        });
     }
 }
