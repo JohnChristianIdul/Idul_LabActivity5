199a774ef4cd32e6c6988731c7df68ee1d2a37f5
JohnChristianIdul <idulchristian@gmail.com>
2022-12-16 19:56:37 +0800

    Add exception handling to SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,6 +3,7 @@ package SimpleCalc;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.InputMismatchException;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel pnMain;
@@ -26,16 +27,26 @@ public class SimpleCalcGUI extends JFrame{
         btnCompute.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (cbOperations.getSelectedItem() == "+") {
-                    tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) + Double.parseDouble(tfNumber2.getText())));
-                } else if (cbOperations.getSelectedItem() == "-") {
-                    tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) - Double.parseDouble(tfNumber2.getText())));
-                } else if (cbOperations.getSelectedItem() == "/") {
-                    if (Double.parseDouble(tfNumber2.getText()) == 0)
-                        throw new ArithmeticException("Cannot divide by zero");
-                    tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) / Double.parseDouble(tfNumber2.getText())));
-                } else if (cbOperations.getSelectedItem() == "*") {
-                    tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) * Double.parseDouble(tfNumber2.getText())));
+                try {
+                    if (cbOperations.getSelectedItem() == "+") {
+                        tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) + Double.parseDouble(tfNumber2.getText())));
+                    } else if (cbOperations.getSelectedItem() == "-") {
+                        tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) - Double.parseDouble(tfNumber2.getText())));
+                    } else if (cbOperations.getSelectedItem() == "/") {
+                        if (Double.parseDouble(tfNumber2.getText()) == 0)
+                            throw new ArithmeticException("Cannot divide by zero");
+                        tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) / Double.parseDouble(tfNumber2.getText())));
+                    } else if (cbOperations.getSelectedItem() == "*") {
+                        tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) * Double.parseDouble(tfNumber2.getText())));
+                    }
+                }catch(ArithmeticException d){
+                    JOptionPane.showMessageDialog(pnMain, d.getMessage());
+                }catch(NullPointerException d){
+                    JOptionPane.showMessageDialog(pnMain, "Every box must be filled");
+                }catch(InputMismatchException d){
+                    JOptionPane.showMessageDialog(pnMain, "Please enter a number");
+                }catch(NumberFormatException d){
+                    JOptionPane.showMessageDialog(pnMain, "Input must not be empty");
                 }
             }
         });
